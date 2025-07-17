12590225c6901843e1abfa6d6612b7faed4b8ce2
leonard-salles <leonardo.sfranca@telefonica.com>
2025-07-17 10:36:15 -0300

    fix: in page not-found added button for routing

--- src/components/navigation/error-page/not-found.jsx
+++ src/components/navigation/error-page/not-found.jsx
@@ -1,7 +1,12 @@
 import Lottie from "react-lottie";
 import notFound from "../../../assets/lottie/404 error page.json";
 
+import { useAuthValue } from "../../../context/auth-context";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
+  const { user } = useAuthValue();
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -13,8 +18,16 @@ export const NotFound = () => {
 
   return (
     <div className="text-center">
-        <Lottie options={defaultOptions} width='80%' height="80%" />
+      <Lottie options={defaultOptions} width="70%" height="70%" />
+      <div className="flex flex-col gap-4 items-center">
         <p className="font-medium text-xl">Parece que não há nada aqui...</p>
+        <Link 
+          to={user ? "/" : "/login"} 
+          className="bg-white text-zinc-900 rounded-lg w-52 p-3 font-medium hover:bg-[#cdcdcd]"
+        >
+          {user ? "Ir a Home" : "Fazer login"}
+        </Link>
+      </div>
     </div>
-  )
+  );
 };
